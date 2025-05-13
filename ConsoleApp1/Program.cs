bbb4e9bc52c63c68241884d9b24d457e40ba5438
Anders T <andwar1992@gmail.com>
2025-05-13 13:37:55 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics.CodeAnalysis;
+using ConsoleApp1;
+
+var returnSum = new ReturnSum();
+
+returnSum.Run();
---
+++ ConsoleApp1/ReturnSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ReturnSum
+    {
+
+
+        int a = 5; 
+        int b = 10;
+
+        public int Sum(int a, int b)
+        {
+            
+                return a + b;
+            }
+        public void Run()
+        {
+            Console.WriteLine($"Dette er sum:{Sum(a,b)} ");
+        }
+
+    }
+
+}
